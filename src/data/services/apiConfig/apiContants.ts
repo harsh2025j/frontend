43328428e05e1587b4e9d205a24d6e22661f52c1
dineshkumartlbupi <dineshkumartlbupi@gmail.com>
2025-12-24 15:06:07 +0530

    change url

--- src/data/services/apiConfig/apiContants.ts
+++ src/data/services/apiConfig/apiContants.ts
@@ -1,6 +1,4 @@
 // export const API_BASE_URL = "https://shellproof-ka-noncorrelative.ngrok-free.dev/";
-
-
 export const API_BASE_URL = "https://api.sajjadhusainlawassociates.com/";
 export const API_ENDPOINTS = {
   AUTH: {
