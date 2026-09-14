12d56b4e9b3ca4e8d451ea181e727bc05735e264
Keshav Pathak <146722954+Keshav301@users.noreply.github.com>
2026-09-14 19:00:05 +0530

    Disable Analytics and SpeedInsights components
    Comment out Analytics and SpeedInsights imports and usage.

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -15,8 +15,8 @@ import Script from 'next/script';
 import GoogleAnalyticsTracker from "@/components/GoogleAnalyticsTracker";
 
 import { API_BASE_URL, API_ENDPOINTS } from "@/data/services/apiConfig/apiContants";
-import { Analytics } from "@vercel/analytics/next";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+// import { Analytics } from "@vercel/analytics/next";
+// import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -266,8 +266,8 @@ export default async function RootLayout({
                 </ClientLayout>
 
                 <GoogleAnalyticsTracker />
-                <Analytics />
-                <SpeedInsights />
+                {/* <Analytics />
+                <SpeedInsights /> */}
 
                 {/* <GlobalLoader /> */}
                 <Toaster
